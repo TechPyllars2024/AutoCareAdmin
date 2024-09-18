b64595789288b99267fd3e09870b53eed6dca082
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-09-19 05:49:10 +0800

    Added Admin Dashboard

---
+++ lib/Dashboard/screens/admin_dashboard.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../widgets/dashboard_sidebar.dart';
+
+class AdminDashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          DashboardSidebar(),
+          const Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    'Dashboard',
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text('Dashboard Content'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
