291a26dcfb3681cc0048ad15118d6825319c8798
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-09-22 03:22:29 +0800

    Added Firebase to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 // import 'package:autocareadmin/Authentication/screens/admin_login.dart';
 import 'package:autocareadmin/Authentication/screens/admin_login.dart';
+import 'package:autocareadmin/Authentication/screens/login_sample.dart';
+import 'package:autocareadmin/Dashboard/screens/verifushop2.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBXEcv6WxwIXp7KyEkI9Xpqkovj3LoGz1U",
+      projectId: "autocare-63060",
+      messagingSenderId: "1000331742478",
+      appId: "1:1000331742478:web:10267f670bbbebb379db5f",
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -18,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: AdminLogin(),
+      home: AdminVerifyShop2(),
     );
   }
 }
