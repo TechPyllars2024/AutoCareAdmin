535ce1e5738710c68cf5daefbdc9c7195fadba38
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2024-09-26 03:05:30 +0800

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:autocareadmin/Authentication/screens/admin_login.dart';
-import 'package:autocareadmin/Dashboard/screens/example.dart';
-// import 'package:autocareadmin/Authentication/screens/login_sample.dart';
 import 'package:autocareadmin/Dashboard/screens/verifushop2.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
